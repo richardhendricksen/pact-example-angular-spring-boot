02c9d561fcee14968d8a5a3806221a72381429f6
richardhendricksen <richard.hendricksen@gmail.com>
2019-11-19 21:18:59 +0100

    added user.service + pact test

--- angular8-consumer/karma.conf.js
+++ angular8-consumer/karma.conf.js
@@ -1,23 +1,27 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const path = require('path');
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'pact'],
     plugins: [
       require('karma-jasmine'),
+      require('@pact-foundation/karma-pact'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        timeoutInterval: 40000
+      }
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-consumer'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
@@ -26,7 +30,18 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    pact: [
+      {
+        cors: true,
+        spec: 3,
+        port: 1234,
+        log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
+        dir: path.resolve(process.cwd(), 'pacts')
+      }
+    ],
+    proxies: {
+      '/user-service/users': 'http://localhost:1234/user-service/users'
+    }
   });
 };
---
+++ angular8-consumer/src/app/user.service.pact.spec.ts
@@ -0,0 +1,142 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './user.service';
+import { User } from './user';
+import { Matchers, PactWeb } from '@pact-foundation/pact-web';
+
+describe('UserServicePact', () => {
+
+  let provider;
+
+  // Setup Pact mock server for this service
+  beforeAll(async () => {
+
+    provider = await new PactWeb({
+      consumer: 'ui',
+      provider: 'userservice',
+      port: 1234
+    });
+
+    // required for slower CI environments
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Required if run with `singleRun: false`
+    await provider.removeInteractions();
+  });
+
+  // Configure Angular Testbed for this service
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        UserService
+      ]
+    });
+  });
+
+  afterAll(function (done) {
+    provider.finalize()
+      .then(function () {
+        done();
+      }, function (err) {
+        done.fail(err);
+      });
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        UserService
+      ],
+    });
+  });
+
+  // Verify test
+  afterEach(async () => {
+    await provider.verify();
+  });
+
+  // Create contract
+  afterAll(async () => {
+    await provider.finalize();
+  });
+
+  describe('create()', () => {
+
+    const expectedUser: User = {
+      firstName: 'Arthur',
+      lastName: 'Dent'
+    };
+
+    const createdUserId = 42;
+
+    beforeAll(async () => {
+      await provider.addInteraction({
+        state: `provider accepts a new person`,
+        uponReceiving: 'a request to POST a person',
+        withRequest: {
+          method: 'POST',
+          path: '/user-service/users',
+          body: expectedUser,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        },
+        willRespondWith: {
+          status: 201,
+          body: Matchers.somethingLike({
+            id: createdUserId
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      });
+    });
+
+    it('should create a Person', async () => {
+      const userService: UserService = TestBed.get(UserService);
+      await userService.create(expectedUser).toPromise().then(response => {
+        expect(response).toEqual(createdUserId);
+      });
+    });
+
+  });
+
+  describe('update()', () => {
+
+    const expectedUser: User = {
+      firstName: 'Zaphod',
+      lastName: 'Beeblebrox'
+    };
+
+    beforeAll(async () => {
+      await provider.addInteraction({
+        state: `person 42 exists`,
+        uponReceiving: 'a request to PUT a person',
+        withRequest: {
+          method: 'PUT',
+          path: '/user-service/users/42',
+          headers: {'Content-Type': 'application/json'},
+          body: Matchers.somethingLike(expectedUser)
+        },
+        willRespondWith: {
+          status: 200,
+          body: Matchers.somethingLike(expectedUser)
+        }
+      });
+    });
+
+    it('should update a Person', async () => {
+      const userService: UserService = TestBed.get(UserService);
+
+      await userService.update(expectedUser, 42).toPromise().then(response => {
+      });
+    });
+  });
+});
